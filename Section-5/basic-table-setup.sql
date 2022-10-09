00682353cc97f9831b53722367707c1c3486015b
cr7yash <yashaqua@gmail.com>
2022-10-09 13:35:17 +0530

    create db sales_example

---
+++ Section-5/basic-table-setup.sql
@@ -0,0 +1,14 @@
+-- create DATABASE sales_example;
+create table sales(
+    -- id INT PRIMARY KEY AUTO_INCREMENT,
+    -- -- mysql
+    id SERIAL PRIMARY KEY,
+    --postgres
+    date_created DATE DEFAULT (CURRENT_DATE),
+    date_fulfilled DATE,
+    customer_name VARCHAR(300) NOT NULL,
+    product_name VARCHAR(300) NOT NULL,
+    volume NUMERIC(10, 3) NOT NULL CHECK (volume >= 0),
+    is_recurring BOOLEAN DEFAULT FALSE,
+    is_disputed BOOLEAN DEFAULT FALSE
+)
